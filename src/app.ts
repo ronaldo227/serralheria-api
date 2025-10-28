d764e94360b96d6b18f91d9c4709d63b15a779f8
ronaldo227 <ronaldo227@github.com>
2025-10-28 14:35:49 -0300

    fix: corrige log da porta do servidor

--- src/app.ts
+++ src/app.ts
@@ -23,9 +23,8 @@ app.use(express.json());
 //app.use('/painel-adm', painelAdmRoutes);
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
 
 //export default app;
